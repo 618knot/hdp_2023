2ae6dea34cde73082fa768f76eb18006c8edac9c
618knot <publicac.clt618knot@gmail.com>
2023-12-14 08:59:44 +0900

    history

--- frontend/src/view/history/History.tsx
+++ frontend/src/view/history/History.tsx
@@ -1,8 +1,21 @@
 import PullDown from "../../components/pulldown/PullDown";
-import "./History.scss"
+import "./History.scss";
+import axios from "../../util/axios_base";
+import { useEffect, useState } from "react";
+
 
 
 const History = () => {
+    const [data, setData] = useState([]);
+    useEffect(() => {
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        axios.get("/history/1").then(
+            (response) => {
+                setData(response.data["history"]);
+            }
+        );
+    }, []);
+
     return (
         <div className="History">
             <h2>履歴</h2>
@@ -26,6 +39,18 @@ const History = () => {
                     <td className="name">研究次郎</td>
                     
                 </tr>
+                {
+                    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                    data.map((item: any) => {
+                        return (
+                            <tr>
+                                <td className="time">{item["time"]}</td>
+                                <td className="status">{item["status"] ? "入室" : "退室"}</td>
+                                <td className="name">{item["user"]}</td>
+                            </tr>
+                        );
+                    })
+                }
             </table>
         </div>
     );
