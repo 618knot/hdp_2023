9666774244d136035d780940dbe274b483ddc1da
618knot <publicac.clt618knot@gmail.com>
2023-12-14 11:58:38 +0900

    dotenv

--- app/sqlite/create_being_status.py
+++ app/sqlite/create_being_status.py
@@ -3,8 +3,12 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.sql import func
 from dotenv import load_dotenv
 import os
+from os.path import join, dirname
 
-load_dotenv()
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 DB_NAME = os.environ.get("DB_NAME")
 
--- app/sqlite/util.py
+++ app/sqlite/util.py
@@ -2,8 +2,12 @@ from sqlalchemy import create_engine, Column, Integer, String, Boolean, DateTime
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 import os
+from os.path import join, dirname
 
-load_dotenv()
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
 DB_NAME = os.environ.get("DB_NAME")
 
